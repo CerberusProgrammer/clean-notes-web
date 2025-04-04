c750d6ac9fa8b7e7fa9b81137a48560cf7eff205
Omar Flores <dev@dev.com>
2025-04-04 08:40:33 -0700

    Agregar userReducer para gestionar el estado de autenticación del usuario

---
+++ src/auth/UserReducer.ts
@@ -0,0 +1,31 @@
+import { UserActions } from "./UserActions";
+import { UserState } from "./UserState";
+
+export const userReducer = (
+  state: UserState,
+  action: UserActions
+): UserState => {
+  switch (action.type) {
+    case "LOGIN_REQUEST":
+      return { ...state, isLoading: true, isError: false };
+    case "LOGIN_SUCCESS":
+      return {
+        ...state,
+        isLoading: false,
+        isAuthenticated: true,
+        user: action.payload.user,
+        isAdmin: action.payload.isAdmin,
+      };
+    case "LOGIN_FAILURE":
+      return {
+        ...state,
+        isLoading: false,
+        isError: true,
+        error: action.payload,
+      };
+    case "LOGOUT":
+      return { ...state, user: null, isAuthenticated: false, isAdmin: false };
+    default:
+      return state;
+  }
+};
