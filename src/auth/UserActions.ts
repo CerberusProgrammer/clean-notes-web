2314f5864b9843edc0cbcc85ddf7951e1fa81aab
Omar Flores <dev@dev.com>
2025-04-04 08:40:20 -0700

    Agregar tipo UserActions para gestionar el estado de autenticación del usuario

---
+++ src/auth/UserActions.ts
@@ -0,0 +1,7 @@
+import { User } from "./User";
+
+export type UserActions =
+  | { type: "LOGIN_REQUEST" }
+  | { type: "LOGIN_SUCCESS"; payload: { user: User; isAdmin: boolean } }
+  | { type: "LOGIN_FAILURE"; payload: string }
+  | { type: "LOGOUT" };
